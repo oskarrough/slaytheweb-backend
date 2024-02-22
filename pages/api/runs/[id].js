28e7f60926d52cd6f4c341e2c419dd0306da748f
Oskar Rough <oskar@rough.dk>
2024-02-22 12:10:25 +0100

    Add /runs/{id} endpoint

---
+++ pages/api/runs/[id].js
@@ -0,0 +1,38 @@
+// import Cors from 'cors'
+import { client, parseData } from '../../../src/db'
+
+// const cors = Cors({
+// 	methods: ['GET', 'POST', 'HEAD'],
+// })
+
+export default async function handler(req, res) {
+	// await runMiddleware(req, res, cors)
+	if (req.method !== 'GET') {
+		return res.status(500, 'only get allowed')
+	}
+	const run = await getRun(req.query.id)
+	return res.status(200).json(run)
+}
+
+async function getRun(id) {
+	const res = await client.execute({
+		sql: `
+			select
+				id,
+				player, 
+				created_at,
+				game_state as gameState,
+				game_past as gamePast
+    	from runs
+			where id = ?
+		`, 
+		args: [id]
+	})
+	const parsed = parseData(res)
+	return parsed.map((run) => {
+		run.gameState = JSON.parse(run.gameState)
+		run.gamePast = JSON.parse(run.gamePast ?? '[]')
+		return run
+	})[0]
+}
+

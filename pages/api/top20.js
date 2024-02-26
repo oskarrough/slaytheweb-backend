ba368174b99d6e016dd9e5c26005cfe836ef4b37
Oskar Rough <oskar@rough.dk>
2024-02-26 13:37:07 +0100

    Add /top20 endpoint with fastest, won runs

---
+++ pages/api/top20.js
@@ -0,0 +1,26 @@
+import { client, parseData } from '../../src/db'
+
+export default async function handler(req, res) {
+	if (req.method !== 'GET') {
+		res.status(400).json({ msg: 'only GET requests allowed' })
+	}
+	const results = await query()
+	return res.status(200).json(results)
+}
+
+// Returns a subset of the data for each run to keep it light(er).
+async function query() {
+	const res = await client.execute(`
+	select
+		id,
+		player,
+		((JSON_EXTRACT(game_state, "$.createdAt") - JSON_EXTRACT(game_state, "$.endedAt")) / 1000 * -1) as seconds
+	from runs
+	where JSON_EXTRACT(game_state, "$.won") is 1
+	order by seconds asc
+	limit 20
+  `)
+	let parsed = parseData(res)
+	return parsed
+}
+
